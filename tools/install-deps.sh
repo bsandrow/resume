8509c2c1dc930336ef8c69bb104490c41ec3a6f0
Brandy Sandrowicz <brandy@sandrowicz.org>
2023-06-29 02:49:51 -0400

    Move TeXLive packages to requirements.txt

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -40,20 +40,6 @@ esac
 # tlmgr - TeX Live Package Manager
 #
 sudo tlmgr update --self
-sudo tlmgr install texliveonfly
-sudo tlmgr install xelatex
-sudo tlmgr install adjustbox
-sudo tlmgr install tcolorbox
-sudo tlmgr install collectbox
-sudo tlmgr install ucs
-sudo tlmgr install environ
-sudo tlmgr install trimspaces
-sudo tlmgr install titling
-sudo tlmgr install enumitem
-sudo tlmgr install rsfs
-
-#
-# tlmgr - additional packages used by resume.tex
-#
-sudo tlmgr install marvosym
-sudo tlmgr install titlesec
+cat requirements.txt | sed '/^\s*$/d; /^\s*#/d;' | while read pkg; do
+    sudo tlmgr install $pkg
+done
